b9323d48abeb9d079a176e53a41cf1166c0ecb4d
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2023-12-01 15:07:02 +0530

    updated

--- script.js
+++ script.js
@@ -6,24 +6,28 @@ fetch("https://restcountries.com/v3.1/all")
             const div = document.createElement("div");
             let lat = `${ele[i].latlng[0]}`;
             let long = `${ele[i].latlng[1]}`;
+            // div.classList.add("container","d-flex" , "flex-row")
             div.setAttribute("class","container")
             div.innerHTML = `
-                <div class="row">
-                    <div class ="col-lg-4 col-sm-12"> 
-                        <div class="card mt-3">
-                            <div class="card-header bg-secondary cardHeader">${ele[i].name.common}</div>                            
-                            <div class="card-body text-center">
-                                <img src="${ele[i].flags.png}" class="card-img-top" alt="Flag">
-                                <p class="card-text"> Capital : ${ele[i].capital}</p>
-                                <p class="card-text"> Region : ${ele[i].region}</p>
-                                <p class="card-text"> Country Code : ${ele[i].subregion}</p>
-                                <p class="card-text"> Population : ${ele[i].population}</p>
-                                <p class="card-text"> Lat & Lng : ${lat} , ${long}</p>
-                                <button type="button" id="button" onclick="weather(${lat},${long})" class="btn btn-primary">Click for Weather</button>
+            <div class = "row">
+                <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 g-5">
+                    <div class="card h-100 w-auto" style="width: 18rem;" id="card">
+                        <div class="card-header text-center" id="country-name">${ele[i].name.common}</div>
+                        <img src="${ele[i].flags.svg}" class="card-img-top" alt='country-cards'>
+                        <div class="card-body">
+                            <div class="card-text"><b>Region : </b>${ele[i].region}</div>
+                            <div class="card-text"><b>Country-code : </b>${ele[i].altSpellings[0]}</div>
+                            <div class="card-text"><b>Capital : </b>${ele[i].capital}</div>
+                            <div class="card-text"><b>Population : </b>${ele[i].population}</div>
+                            <div id="${ele[i].name.common}"></div>
+                        </div>
+                            <div class="card-footer d-flex justify-content-center">
+                            <button type="button" id="button" onclick="weather(${lat},${long})" class="btn btn-primary">Click for Weather</button>
                             </div>
                         </div>
                     </div>
-                </div>`;
+                </div>
+            </div>`;
             document.body.append(div);
         }       
     }
